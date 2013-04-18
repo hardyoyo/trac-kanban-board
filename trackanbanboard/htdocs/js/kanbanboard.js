8dbeb84b32452c998780802bf74555293173a0f7
Joonas Jauhiainen <joonas.jauhiainen@eficode.com>
2013-04-18 07:11:36 +0300

    Kanbanboard should now detect if session expired.

--- trackanbanboard/htdocs/js/kanbanboard.js
+++ trackanbanboard/htdocs/js/kanbanboard.js
@@ -1,3 +1,38 @@
+function getCookie(c_name)
+{
+    var c_value = document.cookie;
+    var c_start = c_value.indexOf(" " + c_name + "=");
+    if (c_start == -1)
+      {
+      c_start = c_value.indexOf(c_name + "=");
+      }
+    if (c_start == -1)
+      {
+      c_value = null;
+      }
+    else
+      {
+      c_start = c_value.indexOf("=", c_start) + 1;
+      var c_end = c_value.indexOf(";", c_start);
+      if (c_end == -1)
+      {
+    c_end = c_value.length;
+    }
+    c_value = unescape(c_value.substring(c_start,c_end));
+    }
+    return c_value;
+};
+
+function checkSession(){
+    var check = true;
+    if(getCookie("trac_auth").length <= 0 || getCookie('trac_form_token') <= 0){
+        check = false;
+        alert("Session expired!");
+    }
+    return check;
+};
+
+
 var kanban = kanban || {};
 
 kanban.Ticket = function(data) {
